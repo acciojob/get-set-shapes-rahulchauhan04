2b38f557530edb5b85f2abe36d9241bd96fb4e7d
Rahul Chauhan <56860601+rahulchauhan04@users.noreply.github.com>
2024-05-15 21:54:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width, height) {
+		this._width = nummber;
+		this._height = number;
+	}
 
-class Square extends Animal {}
+	getArea() {
+		return this._width * this._height;
+	}
+}
+
+class Square extends Animal {
+	getPerimeter() {
+		return 4 * side ;
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
